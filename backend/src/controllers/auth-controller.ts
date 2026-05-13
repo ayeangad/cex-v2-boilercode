39b72a46d3a77ed671649c762bcff8fe9c37009f
Angad <angadsankhla09@gmail.com>
2026-05-13 16:00:56 +0530

    signin backend completed
    /signin route is completeda

--- backend/src/controllers/auth-controller.ts
+++ backend/src/controllers/auth-controller.ts
@@ -4,6 +4,7 @@ import { prisma } from "../db.js";
 import { authSchema } from "../types/auth-schema.js";
 import { createToken } from "../utils/auth.js";
 import { sendValidationError } from "../utils/validation.js";
+import { jwt } from "zod";
 
 export async function signup(req: Request, res: Response): Promise<void> {
   const parsedBody = authSchema.safeParse(req.body);
@@ -35,4 +36,41 @@ export async function signup(req: Request, res: Response): Promise<void> {
 
 export async function signin(req: Request, res: Response): Promise<void> {
   //TODO: Implement signin logic
+  const parsedBody = authSchema.safeParse(req.body);
+  if (!parsedBody.success) {
+    sendValidationError(res, parsedBody.error);
+    return;
+  }
+
+  const { username, password } = parsedBody.data;
+
+  try {
+    const userExists = await prisma.user.findUnique({ where: { username } })
+    if (!userExists) {
+      res.status(403).json({
+        message: "User doesn't exists!"
+      });
+      return;
+    }
+
+    const correctPassword = await bcrypt.compare(password, userExists.password)
+    if (!correctPassword) {
+      res.status(403).json({
+        message: "Incorrect password!"
+      })
+      return;
+    }
+
+    const token = createToken({
+      userId: userExists.id
+    })
+
+    res.json({
+      message: "You've signed in!",
+      token
+    })
+
+  } catch {
+    res.status(401).json({ error: "Invalid credentials" });
+  }
 }
